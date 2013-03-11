9518d165a38688003f27e48ff5c6c7836744b989
Jouni L <jnl@iki.fi>
2013-03-11 20:35:00 +0200

    first

--- include/cb_buffer.h
+++ include/cb_buffer.h
@@ -1,3 +1,19 @@
+/* 
+ * Library to read and write streams. Valuepair indexing with different character encodings.
+ * 
+ * Copyright (C) 2009, 2010 and 2013. Jouni Laakso
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License version 2.1 as published by the Free Software Foundation 6. of June year 2012;
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details. You should have received a copy of the GNU Lesser General Public License along with this library; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * licence text is in file LIBRARY_LICENCE.TXT with a copyright notice of the licence text.
+ */
+
 #define CBSUCCESS            0
 #define CBSTREAM             1
 #define CBMATCH              2
--- include/cb_encoding.h
+++ include/cb_encoding.h
@@ -1,3 +1,19 @@
+/* 
+ * Library to read and write streams. Valuepair indexing with different character encodings.
+ * 
+ * Copyright (C) 2009, 2010 and 2013. Jouni Laakso
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License version 2.1 as published by the Free Software Foundation 6. of June year 2012;
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+ * more details. You should have received a copy of the GNU Lesser General Public License along with this library; if
+ * not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * licence text is in file LIBRARY_LICENCE.TXT with a copyright notice of the licence text.
+ */
+
 #define byteisascii(x)            ( (x|0x7F)==0x7F ) // (x|01111111==01111111)
 #define byteisutf8tail(x)         ( (x|0x3F)==0xBF ) // (x|00111111==10111111)
 #define byteisutf8head2(x)        ( (x|0x1F)==0xDF ) // (x|00011111==11011111)
@@ -5,13 +21,13 @@
 #define byteisutf8head4(x)        ( (x|0x07)==0xF7 ) // (x|00000111==11110111)
 #define byteisutf8head5(x)        ( (x|0x03)==0xFB ) // (x|00000011==11111011)
 #define byteisutf8head6(x)        ( (x|0x01)==0xFD ) // (x|00000001==11111101)
-#define intisutf8bom(x)           ( x==0xEFBBBF )       // 0000 FEFF
-#define intisbombigebom(x)        ( x==0xFEFF )       // 0000 FEFF
-#define intisbomlittleebom(x)     ( x==0xFFFE )       // 0000 FEFF
-#define intisutf16bigebom(x)      ( x==65279 )       // 0000 FEFF
-#define intisutf16littleebom(x)   ( x==65534 )       // 0000 FFFE
-#define bytesareutf8bom(h,l)      ( h==0xFE && l==0xFF ) // 0000 FEFF
-#define bytesareutf16bigebom(h,l)   bytesareutf8bom(h,l) //  0000 FEFF
+#define intisutf8bom(x)           ( x==0xEFBBBF )            // 0000 FEFF
+#define intisbombigebom(x)        ( x==0xFEFF )              // 0000 FEFF
+#define intisbomlittleebom(x)     ( x==0xFFFE )              // 0000 FEFF
+#define intisutf16bigebom(x)      ( x==65279 )               // 0000 FEFF
+#define intisutf16littleebom(x)   ( x==65534 )               // 0000 FFFE
+#define bytesareutf8bom(h,l)      ( h==0xFE && l==0xFF )     // 0000 FEFF
+#define bytesareutf16bigebom(h,l)   bytesareutf8bom(h,l)     // 0000 FEFF
 #define bytesareutf16littleebom(h,l)  ( l==0xFE && h==0xFF ) // 0000 FFFE
 
 #define masktoutf8tail(x)           x = ( ( x & 0x3F ) | 0x80 ) 
